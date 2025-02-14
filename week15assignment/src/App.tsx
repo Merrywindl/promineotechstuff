2a5d2a213004363d1bc8d8dcb3054e166170cf02
Merrywindl <whasup42@hotmail.com>
2025-02-13 22:58:16 -0600

    first committ with comments and complete

---
+++ week15assignment/src/App.tsx
@@ -0,0 +1,131 @@
+import { useEffect, useState } from "react";
+import ListField from "./ListField";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+import SlideView from "./SlideView";
+import Rightinfobar from "./Rightinfobar";
+import thumbnailImage from "./assets/Suzume.jpg";
+import thumbnailImage2 from "./assets/weatheringwithyou.jpg";
+import thumbnailImage3 from "./assets/yourname.jpg";
+import thumbnailImage4 from "./assets/COMING-SOON-.jpg";
+
+
+//my data//
+const testSlides = [
+  {
+    id: 0,
+    text: "Suzume",
+    image: thumbnailImage,
+    story: "abc",
+    fontColor: "Black"
+  },
+  {
+    id: 1,
+    text: "Weathering With You",
+    image: thumbnailImage2,
+    story: "def",
+    fontColor: "Black"
+  },
+  {
+    id: 2,
+    text: "Your Name",
+    image: thumbnailImage3,
+    story: "ghi",
+    fontColor: "Black"
+  },
+  {
+    id: 3,
+    text: "Coming Soon",
+    image: thumbnailImage4,
+    story: "jkl",
+    fontColor: "Black"
+  },
+  {
+    id: 4,
+    text: "Coming Soon",
+    image: thumbnailImage4,
+    story: "mno",
+    fontColor: "Black"
+  },
+  {
+    id: 5,
+    text: "Coming Soon",
+    image: thumbnailImage4,
+    story: "pqr",
+    fontColor: "Black"
+  }
+];
+
+export default function App() {
+  // Array of background images can add more in future used urls
+  const backgroundImages = [
+    "https://images8.alphacoders.com/131/thumb-1920-1310791.png",
+    "https://images6.alphacoders.com/134/thumb-1920-1346530.jpeg",
+  ];
+
+  const [slides, setSlides] = useState(testSlides);
+  const [selectedSlideId, setSelectedSlideId] = useState(0);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const changeBackgroundImage = (url: string) => {
+    document.body.style.backgroundImage = `url(${url})`;
+    document.body.style.backgroundAttachment = 'fixed';
+  };
+
+  // Set the initial background image when the app loads this was an interesting find
+  useEffect(() => {
+    changeBackgroundImage(backgroundImages[0]);
+  }, []);
+
+  const handleImageCycle = () => {
+    const nextIndex = (currentIndex + 1) % backgroundImages.length; //this cycles through the url images with the % indicating to loop around
+    setCurrentIndex(nextIndex);
+    changeBackgroundImage(backgroundImages[nextIndex]);
+  };
+
+  // Applying the background image to the body
+  useEffect(() => {
+    document.body.style.backgroundImage = `url(${backgroundImages[currentIndex]})`; //sets where to find image
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundAttachment = 'fixed'
+  }, [currentIndex]);
+
+  const selectedSlide = slides.find((s) => s.id === selectedSlideId);
+
+  const addBlankSlide = () => {
+    const blankSlide = {
+      id: slides.length > 0 ? slides[slides.length - 1].id + 1 : 0, 
+      text: "Coming Soon",
+      image: thumbnailImage4,
+      story: "pqr",
+      fontColor: "Black"
+    };
+
+    setSlides([...slides, blankSlide]);
+  }
+
+  const deleteSlide = (idToDelete: number) => {
+    setSlides( slides.filter(s => s.id !==idToDelete) ) //delete slide 
+  }
+
+  const updateslideFontColor = ( newColor: string, idToUpdate?: number) => {
+    setSlides(slides.map(slide => (
+      slide.id !== idToUpdate ? slide : {
+        ...slide,
+        fontColor: newColor
+      }
+    )))
+  }
+
+  return (
+    <div className="d-flex flex-column vh-100">
+      <Navbar selectedSlide={selectedSlide}changeBackgroundImage={handleImageCycle} updateslideFontColor={updateslideFontColor}/>
+      <ListField />
+      <div className="d-flex flex-grow-1">
+        <Sidebar addBlankSlide={addBlankSlide} />
+        <SlideView slides={slides} deleteSlide={deleteSlide}selectedSlideId={selectedSlideId} setSelectedSlideId={setSelectedSlideId} /> {/* Use slides here */}
+        <Rightinfobar slide={selectedSlide} />
+      </div>
+    </div>
+  )
+}
---
+++ week15assignment/src/Navbar.tsx
@@ -0,0 +1,78 @@
+import NavBarButton from "./NavBarButtons";
+import Music from "./assets/music-solid.svg";
+import Movies from "./assets/film-solid.svg";
+import Games from "./assets/gamepad-solid.svg";
+import Background from "./assets/background.svg";
+import TextColor from "./assets/fill-solid.svg";
+import { Button, Modal } from "react-bootstrap";
+import { useState } from "react";
+
+type NavbarProps = {
+  changeBackgroundImage: () => void
+  updateslideFontColor: ( color: string, id?: number,) =>  void
+  selectedSlide?: {id: number, text: string, image: string, fontColor: string} 
+}
+
+export default function Navbar({ changeBackgroundImage, updateslideFontColor, selectedSlide }: NavbarProps) {
+  const [isBackgroundModalOpen, setIsBackgroundModalOpen] = useState(false);
+  const [isTextColorModalOpen, setTextColorModalOpen] = useState(false);
+
+  const handleCloseBackgroundModal = () => setIsBackgroundModalOpen(false);
+  const handleCloseTextColorModal = () => setTextColorModalOpen(false);
+  
+
+  return (
+    <div className="bg-transparent p-3 border border-black border-5 d-flex justify-content-center w-100">
+      <div className="bg-transparent p-3 d-flex justify-content-center w-75">
+        <NavBarButton icon={Movies} onClick={() => alert("Coming Soon")} /> {/*nav bar buttons for decoration or future use*/}
+        <NavBarButton icon={Music} onClick={() => alert("Coming Soon")} />
+        <NavBarButton icon={Games} onClick={() => alert("Coming Soon")} />
+      </div>
+      <div className="bg-transparent p-3 border-5 d-flex justify-content-left w-50">
+        <NavBarButton icon={Background} onClick={() => setIsBackgroundModalOpen(true)} />
+
+        <Modal show={isBackgroundModalOpen} onHide={handleCloseBackgroundModal}>
+          <Modal.Header>
+            <Modal.Title>Change Background</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Please choose a new background <br /> 
+            <div>
+              <Button onClick={changeBackgroundImage}> {/*modals for my change background and text color*/}
+                Cycle Image
+              </Button>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="danger" onClick={handleCloseBackgroundModal}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleCloseBackgroundModal}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+
+        <NavBarButton icon={TextColor} onClick={() => setTextColorModalOpen(true)} />
+
+        <Modal show={isTextColorModalOpen} onHide={handleCloseTextColorModal}>
+          <Modal.Header>
+            <Modal.Title>Change Text Color</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            Choose a new text color: <br />
+            <Button variant={selectedSlide?.fontColor === "red" ? "danger" : "outline-danger"} onClick={() => updateslideFontColor("red", selectedSlide?.id)}>Red</Button>
+            <Button variant={selectedSlide?.fontColor === "blue" ? "primary" : "outline-primary"} onClick={() => updateslideFontColor("blue",selectedSlide?.id)} >Blue</Button>
+            <Button variant={selectedSlide?.fontColor === "orange" ? "warning" : "outline-warning"} onClick={() => updateslideFontColor( "orange", selectedSlide?.id)}>Orange</Button>
+          </Modal.Body>
+          <Modal.Footer>
+            
+            <Button variant="success" onClick={handleCloseTextColorModal}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    </div>
+  );
+}
